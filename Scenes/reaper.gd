aaf11ddc9d3df831673dfe3cb160081a5e951ce7
ThunderPlay-Studio <abhishekfarswan117@gmail.com>
2023-01-16 07:48:29 +0530

    enemy character Updates

--- Scenes/reaper.gd
+++ Scenes/reaper.gd
@@ -4,7 +4,7 @@ extends CharacterBody3D
 @onready var eyes = $Eyes
 @onready var Anim = $AnimationPlayer
 
-var Target
+var Target = null
 @export var SPEED = 3
 @export var Turn_speed = 2
 
@@ -16,11 +16,11 @@ enum {
 var state = IDLE
 
 func _physics_process(_delta):
-	var current_location = global_transform.origin
-	var next_location = nav_agent.get_next_location()
-	var new_velocity = (next_location - current_location).normalized() * SPEED
-	
-	nav_agent.set_velocity(new_velocity)
+	if state == WALK:
+		var current_location = global_transform.origin
+		var next_location = nav_agent.get_next_location()
+		var new_velocity = (next_location - current_location).normalized() * SPEED
+		nav_agent.set_velocity(new_velocity)
 
 func Update_target_location(target_location):
 	nav_agent.set_target_location(target_location)
@@ -29,22 +29,20 @@ func _on_navigation_agent_3d_velocity_computed(safe_velocity):
 	velocity = velocity.move_toward(safe_velocity, .25)
 	move_and_slide()
 
-
 func _on_range_body_entered(body):
 	if body.is_in_group("Player"):
 		state = WALK
 		Target = body
-
+		Update_target_location(Target.global_transform.origin)
+		Anim.play("Walk")
 
 func _on_range_body_exited(_body):
-	pass # Replace with function body.
+	state = IDLE
+	Target = null
+	Anim.play("Idle")
 
 func _process(_delta):
 	match state:
-		IDLE:
-			Anim.play("Idle")
-			pass
 		WALK:
 			eyes.look_at(Target.global_transform.origin, Vector3.UP)
 			rotate_y(deg_to_rad(eyes.rotation.y * Turn_speed))
-			Anim.play("Walk")
